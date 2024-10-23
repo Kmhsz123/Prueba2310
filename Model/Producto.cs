a90fa1157db5ab6ba897bc536fec0bd8cef86ecb
KATERIN MARIA HERNANDEZ SELVA <katerin.hernandez22905069@estu.unan.edu.ni>
2024-10-23 15:56:58 -0600

    ¨carpeta models

---
+++ Model/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace Prueba2310.Model
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion  { get; set; }
+
+    }
+}
